2d44bfbe8cc7d59ebd751d29b79d9418d94d2866
lafleur <lafleur@boum.org>
2024-10-28 04:52:58 +0100

    first attempt: copy [deep-link](https://github.com/tauri-apps/plugins-workspace/blob/v2/plugins/deep-link/src/lib.rs)
    when called with a share text event, fails with :
    
    type: crash
    osVersion: google/sunfish/sunfish:13/TQ3A.230805.001.S1/2024092100:user/release-keys
    package: net.coreact.app:1000
    process: net.coreact.app
    processUptime: 223 + 96 ms
    
    java.lang.RuntimeException: Unable to instantiate activity ComponentInfo{net.coreact.app/app.tauri.sharesheet.SharesheetPlugin}: java.lang.InstantiationException: java.lang.Class<app.tauri.sharesheet.SharesheetPlugin> has no zero argument constructor
    	at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:3561)
    	at android.app.ActivityThread.handleLaunchActivity(ActivityThread.java:3792)
    	at android.app.servertransaction.LaunchActivityItem.execute(LaunchActivityItem.java:101)
    	at android.app.servertransaction.TransactionExecutor.executeCallbacks(TransactionExecutor.java:138)
    	at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:95)
    	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2313)
    	at android.os.Handler.dispatchMessage(Handler.java:106)
    	at android.os.Looper.loopOnce(Looper.java:201)
    	at android.os.Looper.loop(Looper.java:288)
    	at android.app.ActivityThread.main(ActivityThread.java:7940)
    	at java.lang.reflect.Method.invoke(Native Method)
    	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:952)
    Caused by: java.lang.InstantiationException: java.lang.Class<app.tauri.sharesheet.SharesheetPlugin> has no zero argument constructor
    	at java.lang.Class.newInstance(Native Method)
    	at android.app.AppComponentFactory.instantiateActivity(AppComponentFactory.java:95)
    	at androidx.core.app.CoreComponentFactory.instantiateActivity(CoreComponentFactory.java:45)
    	at android.app.Instrumentation.newActivity(Instrumentation.java:1344)
    	at android.app.ActivityThread.performLaunchActivity(ActivityThread.java:3548)
    	... 12 more

--- android/src/main/java/SharesheetPlugin.kt
+++ android/src/main/java/SharesheetPlugin.kt
@@ -13,8 +13,15 @@ import android.net.Uri
 import app.tauri.annotation.Command
 import app.tauri.annotation.InvokeArg
 import app.tauri.annotation.TauriPlugin
+import app.tauri.plugin.Channel
+import app.tauri.plugin.JSObject
 import app.tauri.plugin.Invoke
 import app.tauri.plugin.Plugin
+
+@InvokeArg
+class SetEventHandlerArgs {
+    lateinit var handler: Channel
+}
 
 @InvokeArg
 class ShareTextOptions {
@@ -26,6 +33,17 @@ class ShareTextOptions {
 
 @TauriPlugin
 class SharesheetPlugin(private val activity: Activity): Plugin(activity) {
+
+    private var channel: Channel? = null
+    // This command should not be added to the `build.rs` and exposed as it is only
+    // used internally from the rust backend.
+    @Command
+    fun setShareEventHandler(invoke: Invoke) {
+        val args = invoke.parseArgs(SetEventHandlerArgs::class.java)
+        this.channel = args.handler
+        invoke.resolve()
+    }
+
     /**
      * Open the Sharesheet to share some text
      */
@@ -43,5 +61,31 @@ class SharesheetPlugin(private val activity: Activity): Plugin(activity) {
         val shareIntent = Intent.createChooser(sendIntent, null);
         shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.applicationContext?.startActivity(shareIntent);
+    }
+
+    override fun load(webView: WebView) {
+      trigger("load", JSObject())
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/*" == intent.type) {
+                    val event = JSObject()
+                    event.put("mime_type", intent.type)
+                    event.put("data", intent.data.toString())
+                    this.channel?.send(event)
+                    //trigger("newIntent", event)
+                } else if (intent.type?.startsWith("image/") == true) {
+                    println("send image unimplemented")
+                    //handleSendImage(intent)
+                }
+            }
+            intent?.action == Intent.ACTION_SEND_MULTIPLE
+                    && intent.type?.startsWith("image/") == true -> {
+                    println("send images unimplemented")
+                    //handleSendMultipleImages(intent)
+            }
+        }
     }
 }
